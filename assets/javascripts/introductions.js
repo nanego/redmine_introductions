4e49835892c7d6150c209079f51871c12ae9eeb4
Vincent Robert <vincent.robert@nanego.com>
2014-01-30 11:48:09 +0100

    Check if all selectors are present before starting introductions

--- assets/javascripts/introductions.js
+++ assets/javascripts/introductions.js
@@ -4,46 +4,46 @@ function setIntroParams(selector, text, position, step) {
   $(selector).attr('data-step', step);
 }
 
-function startIntroductions(intro_id, next_url, next_step_id) {
-    if(next_url != ""){
-        introJs().setOptions({ 'tooltipClass': "introJsTooltipClass",
-            'showStepNumbers': false,
-            'showButtons': true,
-            'showBullets':false,
-            'doneLabel': 'Page suivante',
-            'skipLabel': 'Passer',
-            'prevLabel': '&larr;',
-            'nextLabel': 'Suivant &rarr;'
-        }).start().oncomplete(function() {
-                window.location.href = next_url + '?multipage=true&intro_id='+intro_id+'&intro_step=' + next_step_id;
-            }).onexit(function() {
-                show_modal_do_not_show_again();
-            });
-    }else{
-        introJs().setOptions({ 'tooltipClass': "introJsTooltipClass",
-            'showStepNumbers': false,
-            'showButtons': true,
-            'showBullets':false,
-            'doneLabel': 'Fermer',
-            'skipLabel': 'Passer',
-            'prevLabel': '&larr;',
-            'nextLabel': 'Suivant &rarr;'
-        }).start().oncomplete(function() {
-                show_modal_do_not_show_again();
-            }).onexit(function() {
-                show_modal_do_not_show_again();
-            });
+function startIntroductions(intro_id, next_url, next_step_id, selectors) {
+    if(validateSelectorsPresence(selectors)){
+        if(next_url != ""){
+            introJs().setOptions({ 'tooltipClass': "introJsTooltipClass",
+                'showStepNumbers': false,
+                'showButtons': true,
+                'showBullets':false,
+                'doneLabel': 'Page suivante',
+                'skipLabel': 'Passer',
+                'prevLabel': '&larr;',
+                'nextLabel': 'Suivant &rarr;'
+            }).start().oncomplete(function() {
+                    window.location.href = next_url + '?multipage=true&intro_id='+intro_id+'&intro_step=' + next_step_id;
+                }).onexit(function() {
+                    show_modal_do_not_show_again();
+                });
+        }else{
+            introJs().setOptions({ 'tooltipClass': "introJsTooltipClass",
+                'showStepNumbers': false,
+                'showButtons': true,
+                'showBullets':false,
+                'doneLabel': 'Fermer',
+                'skipLabel': 'Passer',
+                'prevLabel': '&larr;',
+                'nextLabel': 'Suivant &rarr;'
+            }).start().oncomplete(function() {
+                    show_modal_do_not_show_again();
+                }).onexit(function() {
+                    show_modal_do_not_show_again();
+                });
+        }
     }
 }
 
 jQuery(function() {
-
     $('#introduction_form').on('click', '.remove_fields', function(event) {
         $(this).prev('input[type=hidden]').val('1');
         $(this).closest('li').hide();
         return event.preventDefault();
     });
-
     $('#introduction_form').on('click', '.add_fields', function(event) {
         var regexp, time;
         time = new Date().getTime();
@@ -66,4 +66,14 @@ function updateStepsPriorities() {
 
 function show_modal_do_not_show_again() {
     showModal("ajax-modal", "600px");
+}
+
+function validateSelectorsPresence(selectors) {
+    for(var i = 0; i < selectors.length; i++)
+    {
+        if($(selectors[i]).length<1){
+            return false;
+        }
+    }
+    return true;
 }
