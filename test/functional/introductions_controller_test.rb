da259c2f72621b2fa3009a8b2d049bb27fb3bfae
Vincent Robert <vincent.robert@nanego.com>
2014-03-28 10:52:58 +0100

    Add functionnal tests

--- test/functional/introductions_controller_test.rb
+++ test/functional/introductions_controller_test.rb
@@ -42,8 +42,47 @@ class IntroductionsControllerTest < ActionController::TestCase
     assert_difference('Introduction.count', -1) do
       delete :destroy, :id => Introduction.find(3).to_param
     end
-
     assert_redirected_to introductions_path
+  end
+
+  def test_update_last_view_date
+    @request.session[:user_id] = 2
+    post :update_last_view_date, :format => 'js', :introduction_id => 1
+
+    assert_response :success
+    assert_template 'do_not_show_again'
+    assert_not_nil assigns(:intro_user)
+
+    record = IntroductionsUser.find_by_introduction_id_and_user_id(1,2)
+    assert_not_nil record
+    assert !record.blocked?
+    assert_equal Date.today, record.last_view.to_date
+  end
+
+  def test_do_not_show_again
+    @request.session[:user_id] = 2
+    post :do_not_show_again, :format => 'js', :introduction_id => 1
+
+    assert_response :success
+    assert_template 'do_not_show_again'
+    assert_not_nil assigns(:intro_user)
+
+    record = IntroductionsUser.find_by_introduction_id_and_user_id(1,2)
+    assert_not_nil record
+    assert record.blocked?
+    assert_equal Date.today, record.last_view.to_date
+  end
+
+  def test_show_again
+    # setup
+    IntroductionsUser.find_or_create_by_introduction_id_and_user_id(1, 2)
+    assert_not_nil IntroductionsUser.find_by_introduction_id_and_user_id(1,2)
+
+    post :show_again, :format => 'js', :introduction_id => 1, user_id: 2
+    assert_response :success
+    assert_template 'show_again'
+    assert_nil IntroductionsUser.find_by_introduction_id_and_user_id(1,2)
+    assert_not_nil assigns(:intros_users)
   end
 
 end
