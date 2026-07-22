86646060c4ec55cba817ce99b2a57442f81e9553
Vincent <contact@vincent-robert.com>
2026-07-22 08:43:48 +0200

    Migrate legacy icon classes to SVG sprite_icon for Redmine 7.0

--- app/helpers/introductions_helper.rb
+++ app/helpers/introductions_helper.rb
@@ -6,7 +6,7 @@ module IntroductionsHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render "step_fields", f: builder
     end
-    link_to(name, '#', class: "add_fields icon icon-add", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(sprite_icon('add', name), '#', class: "add_fields icon", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
   def link_to_intro_test(introduction)
